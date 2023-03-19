30280aba9d6691c7ddae8fabb69bd9d8816cf980
ryuichi-maeda <uryu44213@gmail.com>
2023-03-19 19:15:09 +0900

    refactor: to use go-openai package

--- structs/structs.go
+++ structs/structs.go
@@ -1,36 +1,5 @@
 package structs
 
-type Message struct {
-	Role    string `json:"role"`
-	Content string `json:"content"`
-}
-
-type RequestBody struct {
-	Model    string    `json:"model"`
-	Messages []Message `json:"messages"`
-}
-
-type Usage struct {
-	PromptTokens     int `json:"prompt_tokens"`
-	CompletionTokens int `json:"completion_tokens"`
-	TotalTokens      int `json:"total_tokens"`
-}
-
-type Choice struct {
-	Message      Message `json:"message"`
-	FinishReason string  `json:"finish_reason"`
-	Index        int     `json:"index"`
-}
-
-type ResponseBody struct {
-	ID      string   `json:"id"`
-	Object  string   `json:"object"`
-	Created int      `json:"created"`
-	Model   string   `json:"model"`
-	Usage   Usage    `json:"usage"`
-	Choices []Choice `json:"choices"`
-}
-
 type Credentials struct {
 	OpenAIAPIKey string `json:"open_ai_api_key"`
 }
