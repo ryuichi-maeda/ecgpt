d7d9c58d190c245e2f8b80fedf5970982b69cf01
ryuichi-maeda <uryu44213@gmail.com>
2023-03-28 01:45:00 +0000

    fix: behavior

--- config/const.go
+++ config/const.go
@@ -7,7 +7,17 @@ const (
 
 	// File
 	CREDENTIALS_FILE = "credentials.json"
-	BEHAVIOR_FILE    = "behavior.txt"
+
+	// Behavior
+	BEHAVIOR_CONTENT = `
+# Instructions
+You are an American professional English conversation teacher.
+Please chat with me under the following constrains.
+
+# Constrains
+If there is any room for improvement in my English sentence, you advise how to improve it.
+You give a talk thema at the begginning and wait for response.
+Also, you choose the talk thema at random so that the same talk thema does not continue.`
 
 	// Colored Role Output
 	ROLE_OUTPUT_USER      = "\x1b[34m" + "You       > " + "\x1b[0m" // Blue
